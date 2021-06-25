00ebf0c0cc2c8feb57948933eb5611e6be013b58
Timo <timotheus.berg@pm.me>
2021-06-25 11:41:37 +0200

    Working python connection

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from julia import Main
-Main.include("abbild.jl")
+from PIL import Image
+from PIL import ImageShow
+import numpy as np
 
-print(Main.abbild((1, 1, 1)))
+imgobj = Image.open('textures/test.png')
+pixels = imgobj.convert('RGBA')
+data = np.array(pixels)
 
+tuple_data = [tuple(data[x,y,:]) for x in range(data.shape[0]) for y in range(data.shape[1])]
+
+
+Main.include("projection.jl")
+
+
+b = 320
+h = 320
+r = 260
+m = (0, 0, 260+r)
+dichte = 50
+
+projected_array = Main.snapshot_sphere(b, h, tuple_data, m, r, dichte)
+projected_array_int = [tuple(map(int, item)) for sublist in projected_array for item in sublist] 
+
+projected_image = Image.new('RGBA', [500,500])
+projected_image.putdata(projected_array_int)
+ImageShow.show(projected_image)
