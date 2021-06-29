863eaac2350a35a71beba6acc15c53e00d106c3e
timo-berg <timotheus.berg@pm.me>
2021-06-29 12:20:59 +0200

    Code documentation

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ from julia import Main
 from PIL import Image
 from PIL import ImageShow
 import numpy as np
+Main.include("projection.jl")
 
+# Load image and convert it to RGBA
 imgobj = Image.open('textures/ice.jpg')
 pixels = imgobj.convert('RGBA')
 data = np.array(pixels)
 
+# Convert RGBA-arrays to RGBA-tuple
 tuple_data = [tuple(data[x,y,:]) for x in range(data.shape[0]) for y in range(data.shape[1])]
 
-
-Main.include("projection.jl")
-
-
+# Set image and projection parameter
 h = data.shape[0]
 b = data.shape[1]
 r = b//1.5
 m = (0, 0, 260+2*r)
 dichte = 10
 
+# Project image and convert float RGBA to int RGBA
 projected_array = Main.snapshot_sphere(b, h, tuple_data, m, r, dichte)
 projected_array_int = [tuple(map(int, item)) for sublist in projected_array for item in sublist] 
 
+# Plot image
 projected_image = Image.new('RGBA', [500,500])
 projected_image.putdata(projected_array_int)
 ImageShow.show(projected_image)
--- projection.jl
+++ projection.jl
@@ -2,6 +2,15 @@ using Base: Float64
 using Random, Distributions, Images, ImageView, Plots
 
 function abbild(p)
+    """
+    Takes a point of 3d coordinates and returns its 
+    projection onto the z-plane at 250 if the projected
+    point is in the square [-250,250]x[-250,250]
+    
+    p: 3-tuple of numerical values
+
+    return: x,y coordinates of the projected point as tuple
+    """
     if p[3] < 250
         return nothing
     end
@@ -15,6 +24,15 @@ function abbild(p)
 end
 
 function is_visible(p, m, r)
+    """
+    Determines whether a given point is occluded by the sphere.
+
+    p: point under consideration, 3-tuple of numerical values
+    m: center of the sphere, 3-tuple of numerical values
+    r: radius of the sphere, numerical value
+
+    return: true if it is not occluded, false if it is
+    """
     α, β, γ = p
     x, y, z = m
 
@@ -36,16 +54,18 @@ function is_visible(p, m, r)
         end
     end
 
+    # Intersect of line through the origin with the plane
     t = 250/p[3]
 
+    # Intersections of line through the origin with the sphere
     s_1 = (-b+sqrt(b^2-4*a*c))/(2*a)
     s_2 = (-b-sqrt(b^2-4*a*c))/(2*a)
 
     intersect1 = 1 >= round(s_1, digits=3) > t
     intersect2 = 1 >= round(s_2, digits=3) > t
 
-    # If root is zero, it lies on the visibel part of the sphere (only one
-    # intersect) if not, it has two intersects and is therefore on the backside
+    # Point is only visible if it has one point of 
+    # intersection with the sphere
     if intersect1 && intersect2
         return false
     elseif intersect1
@@ -58,11 +78,26 @@ function is_visible(p, m, r)
 end
 
 function samples(x, y, b, h, m, r, dichte)
+    """
+    Creates an array of coordinates on the sphere sampled
+    around the projected point on the sphere.
+
+    x: x-coordinate of the image, integer
+    y: y-coordinate of the image, integer
+    b: width of the image, integer
+    h: height of the image, integer
+    m: center of the sphere, 3-tuple of numerical values
+    r: radius of the sphere, numerical value
+    dichte: number of samples, numerical value
+
+    return: array of sampled points, Array{Tuple{Float64,Float64,Float64} x dichte
+    """
+    # Sample x and y deviations
     δ = rand(Uniform(-1,1), dichte, 2)
-    # δ = zeros(dichte, 2)
     sample_array = Array{Tuple{Float64,Float64,Float64},1}(undef,dichte)
 
     for i in 1:dichte
+        # Project point with deviations onto the sphere
         θ = (x+δ[i,1])*(π/h)
         ϕ = (y+δ[i,2])*2*(π/b)
         sample_array[i] = sphere_projection(θ, ϕ, m, r)
@@ -72,13 +107,38 @@ function samples(x, y, b, h, m, r, dichte)
 end
 
 function sphere_projection(θ, ϕ, m, r)
+    """
+    Project point onto the sphere
+
+    θ: polar angle, numerical value
+    ϕ: azimuth angle, numerical value
+    m: center of the sphere, 3-tuple of numerical values
+    r: radius of the sphere, numerical value
+
+    return: coordinates of point on sphere, 3-tuple of floats
+    """
     x, y, z = m
     x-r*cos(θ), y-r*sin(θ)*sin(ϕ), z+r*sin(θ)*cos(ϕ)
 end
 
 function snapshot_sphere(b, h, daten, m, r, dichte)
+    """
+    Iterates over pixels from daten, samples dichte amount of points on 
+    the sphere per pixel, checks their visibility and projects them onto
+    the plane.
+
+    b: width of the image, integer
+    h: height of the image, integer
+    daten: list of RGBA values, 4-tuple of numerical values between 0 and 255
+    m: center of the sphere, 3-tuple of numerical values
+    r: radius of the sphere, numerical value
+    dichte: number of samples per pixel, numerical value
+
+    return: projected image in RGBA, Array{Tuple{Float64,Float64,Float64,Float64}} of 500x500
+    """
     # Projected plane: 500x500 array of RGBA-tuple
     bild_ebene = Array{Tuple{Float64,Float64,Float64,Float64}}(undef, 500, 500)
+    # Counter array for online averaging
     bild_counter = zeros(Int, 500, 500)
     # Iterate over pixel
     for l in 1:(b*h)
@@ -97,6 +157,7 @@ function snapshot_sphere(b, h, daten, m, r, dichte)
             # Write pixel value at the projected place
             bild_counter[x_plane+249, y_plane+249] += 1
             old_value = collect(bild_ebene[x_plane+249, y_plane+249])
+            # Take online average
             new_value = old_value + (collect(daten[l])-old_value)/bild_counter[x_plane+249, y_plane+249]
             bild_ebene[x_plane+249, y_plane+249] = tuple(new_value...)
         end
@@ -107,6 +168,9 @@ end
 
 
 function sample_z(b,h,m,r,dichte)
+    """
+    Sample x coordinates of image
+    """
     z_values = zeros(Float64, h, b)
     for x in 1:h
         for y in 1:b
@@ -118,6 +182,9 @@ function sample_z(b,h,m,r,dichte)
 end
 
 function sample_visible(b,h,m,r,dichte)
+    """
+    Sample visibility of image
+    """
     visible = zeros(Bool, h, b)
     for x in 1:h
         for y in 1:b
